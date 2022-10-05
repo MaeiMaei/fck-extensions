982ba33231299a5011c0495d4521dcc73907ea83
MaeiMaei <maemaeiscutegirl@gmail.com>
2022-10-05 02:35:17 +0200

    Fuck you.

---
+++ fuckyou.ps1
@@ -0,0 +1,5 @@
+Add-AppxPackage -Path Microsoft.WebpImageExtension_1.0.52351.0_x64__8wekyb3d8bbwe.Appx
+Add-AppxPackage -Path Microsoft.HEIFImageExtension_1.0.50272.0_x64__8wekyb3d8bbwe.Appx
+Add-AppxPackage -Path Microsoft.HEVCVideoExtension_2.0.51121.0_x64__8wekyb3d8bbwe.Appx
+Add-AppxPackage -Path Microsoft.VP9VideoExtensions_1.0.52451.0_x64__8wekyb3d8bbwe.Appx
+Add-AppxPackage -Path Microsoft.WebMediaExtensions_1.0.42192.0_neutral_~_8wekyb3d8bbwe.AppxBundle
